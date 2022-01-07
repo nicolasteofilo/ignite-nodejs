eb5ce33d5c25d09a9553960b93f6d70adfdb43bd
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-07 17:38:24 -0300

    fix: criando estrutura do projeto

---
+++ fundamentos-nodejs/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'Hello World'
+    });
+})
+
+app.listen(3333, () => {
+  console.log("⚙️ Server is running in http://localhost:3333");
+});
