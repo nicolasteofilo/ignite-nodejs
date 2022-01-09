0485044b09089c0a152a3186dff3c657c38f5306
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-09 12:37:48 -0300

    fix: add middlewares

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -7,6 +7,20 @@ app.use(express.json())
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.body;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Customer not found' });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
 
@@ -29,19 +43,14 @@ app.post('/account', (request, response) => {
 
     return response.status(201).json({
         message: 'Customer created successfully!',
+        customers,
     })
 })
 
-app.get('/statement', (request, response) => {
-    const { cpf } = request.header
-    
-    const customer = customers.find(customer => customer.cpf === parseInt(cpf))
-
-    if(!customer) {
-        return response.status(400).json({error: "Customer not found"})
-    }
-
-    return response.json(customer.statement)
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  
+  return res.json(customer.statement);
 })
 
 app.listen(3333, () => console.log('Server on!'))
