0bef40f9403527215d885d62cd3b10ee589c9d78
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-16 17:57:30 -0300

    fix: fineshed challenge 'conceitos do nodejs'

--- challenges/conceitos-do-nodejs/src/index.js
+++ challenges/conceitos-do-nodejs/src/index.js
@@ -8,34 +8,95 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+    const { username } = request.headers;
+    const user = users.find((user) => user.username === username);
+    if(!user){
+        return response.status(404).json({ error: "User not found!"})
+    }
+    request.user = user;
+    return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+    const { name, username } = request.body;
+    const userAlreadyExists = users.some(
+      (user) => user.username === username
+     );
+    if(userAlreadyExists) {
+        return response.status(400).json({ error: "User already exists!"})
+    }
+    const user = {
+      id: uuidv4(),
+      name,
+      username,
+      todos: []
+    }
+    users.push(user)
+    return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.status(200).json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(todo);
+  return response.status(201).json(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params; 
+  user.todos.map(todo => {
+    if(todo.id === id) {
+      todo.title = title
+      todo.deadline = new Date(deadline)
+      return response.status(200).json(todo);
+    } 
+  });
+  return response.status(404).json({ error: "Todo not found!"})
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  user.todos.map(todo => {
+    if(todo.id === id) {
+      todo.done = true
+      return response.status(200).json(todo);
+    } 
+  });
+  return response.status(404).json({ error: "Todo not found!"})
 });
 
+
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  user.todos.map((todo, index) => {
+    if(todo.id === id) {
+      user.todos.splice(todo.id, 1);
+      return response.status(204).json(todo);
+    } 
+  });
+
+  return response.status(404).json({ error: "Todo doesn't exists" });
 });
 
 module.exports = app;
--- challenges/conceitos-do-nodejs/src/server.js
+++ challenges/conceitos-do-nodejs/src/server.js
@@ -1,5 +1,3 @@
 const app = require('./');
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-});
+app.listen(3333);
