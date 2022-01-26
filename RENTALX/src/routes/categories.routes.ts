47ed5b4dddebe96d1406f992419dd6bfa3d1055d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-26 15:32:36 -0300

    fix: insert types in category

--- RENTALX/src/routes/categories.routes.ts
+++ RENTALX/src/routes/categories.routes.ts
@@ -1,22 +1,26 @@
 import { Request, Response, Router } from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRouter = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRouter.post("/", (request: Request, response: Response) => {
     const { name, description } = request.body;
 
-    const category = {
-        id: uuidv4(),
+    const category = new Category();
+
+    // atribuindo itens para dentro do meu objeto category
+    Object.assign(category, {
         name,
         description,
-    }
+        created_at: new Date()
+    })
 
     categories.push(category);
 
-    return response.status(201).json({ messge: 'Category created successfully' });
+    return response.status(201).json({ messge: 'Category created successfully', category });
 })
 
 export { categoriesRouter }
