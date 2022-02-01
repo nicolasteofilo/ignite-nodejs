43939ef5387bf48dd06a484395004add6ff464c0
Nicolas Teofilo <nicolasteofilodecastro@gmail.com>
2022-02-01 20:08:07 -0300

    feat: create file upload

--- RENTALX/src/routes/categories.routes.ts
+++ RENTALX/src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRouter = Router();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRouter.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +15,12 @@ categoriesRouter.post('/', (request, response) => {
 
 categoriesRouter.get('/', (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+categoriesRouter.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 export { categoriesRouter };
