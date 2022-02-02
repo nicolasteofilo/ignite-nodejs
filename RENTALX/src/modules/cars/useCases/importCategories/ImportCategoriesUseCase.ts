5831ae196b00c7d55cfcaff1fc4499817cd73b2e
Nicolas Teofilo <nicolasteofilodecastro@gmail.com>
2022-02-01 21:11:33 -0300

    fix: using node stream

--- RENTALX/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ RENTALX/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,6 +1,18 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoriesUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    //  o ipe pega o que está dentro do nosso  strem e joga para um lugar que determinarmos
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      await console.log(line);
+    });
   }
 }
 
