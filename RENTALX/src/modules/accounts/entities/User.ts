f2f6330b290fb049e7471fb15ae4845be5cca034
Nicolas Teófilo <nicolasteofilo@pop-os.localdomain>
2022-02-09 22:12:01 -0300

    chore: create migration users

---
+++ RENTALX/src/modules/accounts/entities/User.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+class User {
+
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    username: string
+
+    @Column()
+    password: string
+
+    @Column()
+    email: string
+
+    @Column()
+    driver_license: string;
+
+    @Column()
+    isAdmin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+
+export { User }
