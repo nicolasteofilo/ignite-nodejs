3ef2dc14a5eea537f8c4c360807a236237602d3d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-02-14 15:06:31 -0300

    docs: update

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const customers = [];
 
