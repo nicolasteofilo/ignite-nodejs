a30f13e1970a6263da83dce06f3b7a5a922926e5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-02-18 20:19:59 -0300

    feat: :card_file_box: entity car and seed admin

--- RENTALX/src/shared/infra/http/server.ts
+++ RENTALX/src/shared/infra/http/server.ts
@@ -4,14 +4,15 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/infra/typeorm';
 import '@shared/container';
 
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
