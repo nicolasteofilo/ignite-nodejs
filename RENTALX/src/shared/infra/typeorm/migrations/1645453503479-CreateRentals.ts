9475b6719c525650739ad8347e19404f330f3973
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-02-21 11:45:42 -0300

    chore: :card_file_box: create migration rental

---
+++ RENTALX/src/shared/infra/typeorm/migrations/1645453503479-CreateRentals.ts
@@ -0,0 +1,75 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRentals1645453503479 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'rentals',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'start_date',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'end_date',
+            type: 'timestamp',
+          },
+          {
+            name: 'expect_return_date',
+            type: 'timestamp',
+          },
+          {
+            name: 'total',
+            type: 'numeric',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCarRental',
+            referencedTableName: 'cars', // tabela de origem
+            referencedColumnNames: ['id'], // coluna de origem
+            columnNames: ['car_id'], // coluna de destino (referenciada)
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+          {
+            name: 'FKUserRental',
+            referencedTableName: 'users', // tabela de origem
+            referencedColumnNames: ['id'], // coluna de origem
+            columnNames: ['user_id'], // coluna de destino (referenciada)
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('rentals');
+  }
+}
